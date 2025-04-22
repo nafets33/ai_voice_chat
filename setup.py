802a5da390140ba1bf460768e0b2f0b0a335c7bd
nafets33 <stapinski89@gmail.com>
2025-04-22 19:40:12 -0400

    Fix

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ai_voice_llmchat",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # any dependencies like 'requests', 'numpy', etc.
+    ],
+    author="Stefan Stapinski",
+    description="Chat Window & voice component that hangles conversation",
+    url="https://github.com/nafets33/ai_voice_chat",  # Update if on GitHub
+    classifiers=[
+        "Programming Language :: Reach",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
