bfdd2b4295d304f7c0a1c14d80972688e13023f8
nafets33 <stapinski89@gmail.com>
2025-09-01 21:27:49 -0400

    fix init build

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except FileNotFoundError:
 
 setup(
     name="streamlit-ai-voice-chat",
-    version="0.1.0",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=[
         "streamlit>=1.0.0",
